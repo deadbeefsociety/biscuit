89f8e82ea48fb49597875f841d7544827f1dc5ef
Haoxi Tan <haoxi.tan@gmail.com>
2022-12-24 19:37:12 +1300

    prints stack addrs

--- biscuit/user/c/hello.c
+++ biscuit/user/c/hello.c
@@ -9,6 +9,12 @@ int main(int argc, char **argv)
 		for (j = 0; j < 100000000; j++)
 			asm volatile("":::"memory");
 	}
+	// stack addresses
+	char buf[20] =  {0,};
+	// register size_t stackptr asm("rsp");
+	printf("i at: %p buf at: %p\n", &i, &buf);
+
+
 
 	return 0;
 }
